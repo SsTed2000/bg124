eb1332beb699d625390bf5e7d37e49769ee89a68
SsTed <69528856+SsTed2000@users.noreply.github.com>
2020-12-21 16:19:30 +0100

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,7 @@
+@echo off
+color a
+:start
+title %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto :start
